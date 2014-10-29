b0754d2ac32ad745ae8aec4f6a12d659fa2f9638
Scott Persinger <scottpersinger@gmail.com>
2014-10-29 13:40:19 -0700

    Bumped version to 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='salesforce-python-toolkit',
-    version='0.1.3',
+    version='0.1.4',
     description='A fork of http://code.google.com/p/salesforce-python-toolkit/',
     url='http://github.com/BayCitizen/salesforce-python-toolkit',
     packages=[
