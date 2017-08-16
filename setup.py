7d21e2833bcf7895bdbbf401b2943ccba59cfca2
Jeremy West <jeremy@heroku.com>
2017-08-16 14:29:23 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='salesforce-python-toolkit',
-    version='0.1.4',
+    version='0.1.5',
     description='A fork of http://code.google.com/p/salesforce-python-toolkit/',
     url='http://github.com/BayCitizen/salesforce-python-toolkit',
     packages=[
